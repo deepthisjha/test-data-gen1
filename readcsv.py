f94226e00970eaad44011a0b5e5426275400d717
deepthi <deepthisjha@gmail.com>
2022-09-05 13:38:23 +0200

    db connect with random generated data

--- readcsv.py
+++ readcsv.py
@@ -1,6 +1,6 @@
 import csv
 import psycopg2
-
+import random
 
 with open('data.csv', mode='r') as file:
     csvFileReader = csv.reader(file)
@@ -21,14 +21,20 @@ with open('data.csv', mode='r') as file:
     print("Currently in DB")
     print(records)
 
-
     # displaying the contents of the CSV file
     for line in csvFileReader:
         print(line)
-        cursor.execute("insert into customer (id, name,product) values (%s, %s, %s)", line)
+        cursor.execute("insert into customer (id, name,product) values(%s, %s, %s)", line)
+        # Add 10 random orders for a customer
+    # cursor.execute("create table order(order_id int PRIMARY KEY, order_value int)")
+    for i in range(0, 10):
+        randomInts = [random.randint(1, 999999999), random.randint(1, 999999999)]
+        cursor.execute("insert into order_summary(order_id,order_value) values(%s,%s)", randomInts)
 
+    cursor.execute(''' select * from order_summary ''')
+    records = cursor.fetchall()
+
+    print("Currently in DB")
+    print(records)
     con.commit()
     con.close()
-
-
-
